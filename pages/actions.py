68d378ace891d3f8e026734efe60b8c1a39a4bea
Abdelali Laatamli <abdelalilaatamli@gmail.com>
2023-08-30 17:01:50 +0100

    add drive pie plot

--- pages/actions.py
+++ pages/actions.py
@@ -25,5 +25,16 @@ start_date , end_date = set_dates_range(st)
 if start_date or end_date:
     query = f"SELECT * FROM actions_table WHERE date BETWEEN '{start_date}' AND '{end_date}'"
     data = connection.query(query)
+
+    query = f"SELECT * FROM drive_stats WHERE date BETWEEN '{start_date}' AND '{end_date}'"
+    data_devices = connection.query(query)
+
+st.dataframe(data_devices)
     
 set_map_plot(st,data)
+
+
+device_type = data_devices[['device_type' , 'total_count']].groupby(['device_type'])['total_count'].sum().reset_index().head(10).sort_values(by=["total_count"],ascending=True)
+fig = px.pie(device_type, names='device_type', values='total_count', hole=0.5)
+fig.update_layout(title='Device type')
+st.plotly_chart(fig)
